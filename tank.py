e07e2b5939e3ebaeecad924a6b5b7286fcb13da6
Dominik <83286569+Dominik0409@users.noreply.github.com>
2021-08-27 20:08:43 +0200

    27.08.21.2

--- tank.py
+++ tank.py
@@ -1,26 +1,32 @@
 import pygame
-import math as m
+import random
 class Ball():
     def __init__(self,game):
         self.game = game
-        self.ball = pygame.Rect(10,400,40,40)
+        self.ball = []
         self.xv0 = 2
         self.yv0 = 10
         self.g = 9.8
         self.t = 0 
     def draw(self):
         self.update()
-        pygame.draw.circle(self.game.display,(30,40,40),(self.ball.centerx,self.ball.centery),20)
+        for i in self.ball:
+            pygame.draw.circle(self.game.display,(30,40,40),(i[0].centerx,i[0].centery),20)
+            self.game.drawtext(20,self.game.font,str(i[4]),'white',i[0].x+13,i[0].y+5)
     def update(self):
-        if self.ball.right > 800: self.xv0 *= -1
-        if self.ball.left < 0: self.xv0 *= -1
-        if self.ball.bottom > 720:
-            self.t = 0
-        self.ball.x += self.xv0
-        self.yv = self.yv0 -self.g*self.t
-        self.t += 1/60
-        print(self.yv)
-        self.ball.y -= self.yv
+        for i in range(len(self.ball)):
+            if self.ball[i][0].right > 800: self.ball[i][2] *= -1
+            if self.ball[i][0].left < 0: self.ball[i][2] *= -1
+            if self.ball[i][0].bottom > 720:
+                self.ball[i][1] = 0
+            self.ball[i][0].x += self.ball[i][2]
+            yv = self.ball[i][3] -self.g*self.ball[i][1]
+            self.ball[i][1] += 1/60
+            print(yv)
+            self.ball[i][0].y -= yv
+    def spawn(self):
+        if random.randint(0,100) == 1:
+            self.ball.append([pygame.Rect(random.randint(100,700),400,40,40),0,2,10,5])
         
 class Bullets():
     def __init__(self,game):
@@ -32,15 +38,27 @@ class Bullets():
         if self.game.space:
             if self.cooldown == 1:
                 self.bullets_list.append(pygame.Rect(x,y,10,10))
-                self.cooldown = 5
+                self.cooldown = 6
             else: self.cooldown -= 1
     def animate_bullets(self):
         i = 0
         while i < len(self.bullets_list):
             self.bullets_list[i].y -= 10
             self.game.display.blit(self.bullet_png,(self.bullets_list[i].x,self.bullets_list[i].y))
-            if self.bullets_list[i].y < 0 or self.bullets_list[i].colliderect(self.game.ball.ball):
-                self.bullets_list.pop(i)         
+            for j in range(len(self.game.ball.ball)):
+                if self.bullets_list[i].colliderect(self.game.ball.ball[j][0]):
+                    self.bullets_list.pop(i)
+                    if self.game.ball.ball[j][4] != 1: self.game.ball.ball[j][4] -= 1
+                    elif self.game.ball.ball[j][4] == 1:
+                        self.game.ball.ball.pop(j) 
+                        self.game.score += 1
+                    break
+            i += 1
+        i = 0
+        while i < len(self.bullets_list):
+            if self.bullets_list[i].y < 0:
+                self.bullets_list.pop(i)
+                break
             i += 1
             
         
@@ -48,7 +66,7 @@ class Cannon():
     
     def __init__(self,game):
         self.game = game        
-        self.tank = pygame.Rect(400,644,80,80)
+        self.tank = pygame.Rect(400,660,60,60)
         self.tank_png = pygame.image.load("resources/tank.png").convert_alpha()
         self.tank_png = pygame.transform.scale(self.tank_png,(80,80))
         
@@ -72,35 +90,55 @@ class CannonGame():
         self.can = Cannon(self)
         self.bul = Bullets(self)
         self.ball = Ball(self)
+        self.score = 0
         self.grass1_png = pygame.image.load("resources/grass1.png").convert_alpha()
         self.grass1_png = pygame.transform.scale(self.grass1_png,(80,80))
-        self.font = pygame.font.Font("resources/Font1.ttf",30)
+        self.font = "resources/Font1.ttf"
         self.left, self.right, self.space = False,False,False
         
         
     def loop(self):
         self.can.update()
-        self.bul.fire(self.can.tank.centerx-4,self.can.tank.y)
+        self.bul.fire(self.can.tank.centerx+5,self.can.tank.y)
         self.display.fill((184,234,242))
+        pygame.draw.rect(self.display,'red',pygame.Rect(0,0,800*(self.score/100),20))
         self.ball.draw()
         for i in range(10):
             self.display.blit(self.grass1_png, (i*80,720))
             
         self.bul.animate_bullets()
-        self.display.blit(self.can.tank_png, (self.can.tank.x,self.can.tank.y))
+        self.display.blit(self.can.tank_png, (self.can.tank.x,self.can.tank.y-16))
+        self.ball.spawn()
         self.collision()
+        self.isover()
     
     def collision(self):
-        if self.ball.ball.colliderect(self.can.tank):
+        for i in range(len(self.ball.ball)):
+            if self.ball.ball[i][0].colliderect(self.can.tank):
+                self.gameover()
+    def isover(self):
+        if self.score == 100:
             self.gameover()
     
     def gameover(self):
         self.pause = True
         self.display.fill((184,234,242))
-        text = self.font.render("Game Over",True,(255,255,255))
+        self.drawtext(30,self.font,'Game Over','white',400,400)
+        
+    def reset(self):
+        self.can = Cannon(self)
+        self.bul = Bullets(self)
+        self.ball = Ball(self)
+        self.left, self.right, self.space = False,False,False
+        self.pause = False
+    
+    def drawtext(self,size,font,text,color,x,y):
+        fontwsize = pygame.font.Font(font,size)
+        text = fontwsize.render(text,True,color)
         text_rect = text.get_rect()
-        text_rect.topleft = (400,400)
+        text_rect.topleft = (x,y)
         self.display.blit(text,text_rect)
+        
         
         
         
@@ -130,6 +168,12 @@ class CannonGame():
                 self.loop()
                 pygame.display.flip()
                 self.clock.tick(60)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        self.reset()
         
 if __name__ == "__main__":
     game = CannonGame()
